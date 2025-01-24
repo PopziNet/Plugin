ea3802e81cecc92003e969401e741aa656059746
Popzi <tburtun@gmail.com>
2025-01-24 20:56:52 +0000

    Bugfix

--- src/main/java/net/popzi/mechanics/Mechanics.java
+++ src/main/java/net/popzi/mechanics/Mechanics.java
@@ -36,25 +36,24 @@ public class Mechanics {
         LivingEntity entity = e.getEntity(); // Living, apparently
         Player killer = entity.getKiller(); // Null if not killed by a player
 
-        assert killer != null;
-        assert entity instanceof Player;
+        if (killer != null && entity instanceof Player) {
+            ItemStack weapon = killer.getInventory().getItemInMainHand();
 
-        ItemStack weapon = killer.getInventory().getItemInMainHand();
+            if (ITEMS_SWORDS.isTagged(weapon.getType())) {
+                double number = Math.random(); // 0.0 -- to -- 1.0
+                double dropchance = 0.007;
 
-        if (ITEMS_SWORDS.isTagged(weapon.getType())) {
-            double number = Math.random(); // 0.0 -- to -- 1.0
-            double dropchance = 0.007;
+                if (weapon.getEnchantments().get(Enchantment.SWEEPING_EDGE) != null)
+                    dropchance = 0.04;
 
-            if (weapon.getEnchantments().get(Enchantment.SWEEPING_EDGE) != null)
-                dropchance = 0.04;
-
-            if (dropchance > number) { // A drop is to occur.
-                ItemStack head = ItemStack.of(Material.PLAYER_HEAD);
-                SkullMeta meta = (SkullMeta) head.getItemMeta();
-                assert meta != null;
-                meta.setOwningPlayer((OfflinePlayer) entity);
-                head.setItemMeta(meta);
-                Objects.requireNonNull(entity.getLocation().getWorld()).dropItemNaturally(entity.getLocation(), head);
+                if (dropchance > number) { // A drop is to occur.
+                    ItemStack head = ItemStack.of(Material.PLAYER_HEAD);
+                    SkullMeta meta = (SkullMeta) head.getItemMeta();
+                    assert meta != null;
+                    meta.setOwningPlayer((OfflinePlayer) entity);
+                    head.setItemMeta(meta);
+                    Objects.requireNonNull(entity.getLocation().getWorld()).dropItemNaturally(entity.getLocation(), head);
+                }
             }
         }
     }
